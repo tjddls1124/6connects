88d5c44962914f51598a617ec3d52309531c6fa3
sungHyun <ciih468@naver.com>
2018-08-05 20:29:04 +0900

    Minimax 추가 및 Minimax에 필요한 함수들 부분수정

--- AI_Algorithm_Code.cpp
+++ AI_Algorithm_Code.cpp
@@ -39,10 +39,15 @@ int showBoard(int x, int y) : [x, y] 좌표에 무슨 돌이 존재하는지 보여주는 함수 (
 #include "Connect6Algo.h"
 #define width 19
 #define height 19
+#define depth 4			//minimax tree에서 진행하는 단계의 수
+#define childnum 3		//minimax tree에서 만들 자식노드의 수(전장탐색에서 고려할 돌의 수)
+
 // "샘플코드[C]"  -> 자신의 팀명 (수정)
 // "AI부서[C]"  -> 자신의 소속 (수정)
 // 제출시 실행파일은 반드시 팀명으로 제출!
 char info[] = { "TeamName:웅성,Department:한양대학교" };
+
+int max_score;	//Minimax에서 사용하는 최고점 저장하는 전역변수
 
 
 typedef struct {
@@ -53,8 +58,7 @@ typedef struct {
 
 int tempBoard[width][height];
 Coord blocking[10];
-Coord battleTop[10];
-
+Coord battleTop[10][2];		//전장탐색을 하여 가장 좋은 점수가 나온 2개의 돌을 높은 순서대로 저장하는 배열.(cnt가 1이면 한 개의 돌)
 
 
 void myturn(int cnt);
@@ -73,12 +77,14 @@ void shapeScore();//판의 점수를 관리하는 함수
 void myturn(int cnt) {
 
 	int x[2], y[2];
-	saveBoard();
-	
+
+	saveBoard();						//tempboard에 현재 판을 저장.
 	// 이 부분에서 알고리즘 프로그램(AI)을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하시면 됩니다.
 	// 현재 Sample code의 AI는 Random으로 돌을 놓는 Algorithm이 작성되어 있습니다.
 
-	
+	max_score = -10000;
+	Minimax(0, 0, 0);
+
 
 
 	// 이 부분에서 자신이 놓을 돌을 출력하십시오.
@@ -87,14 +93,15 @@ void myturn(int cnt) {
 	domymove(x, y, cnt);
 }
 
-void saveBoard(){ // 현재 board를 tempBoard에 저장
-	for (int i = 0; i < 19; i++){
-		for (int j = 0; j < 19; j++)
+// 현재 board를 tempBoard에 저장
+void saveBoard() {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++)
 			tempBoard[i][j] = board[i][j];
 	}
 }
 
-int tempIsFree(int x, int y){
+int tempIsFree(int x, int y) {
 	return x >= 0 && y >= 0 && x < width && y < height && tempBoard[x][y] == 0;
 }
 
@@ -103,27 +110,33 @@ int tempIsFree(int x, int y){
 turn 1 : my turn
 turn 2 : op turn
 */
-void tempMyMove(int x, int y, int turn){
-	if(tempIsFree)
+void tempMyMove(int x, int y, int turn) {
+	if (tempIsFree)
 		tempBoard[x][y] = turn;
 }
 
-
-void checkBlocking(){ // 블로킹들의 위치저장 , 구조체 배열에 저장
-	int count = 0;
-	for (int i = 0; i < 19; i++){
-		for (int j = 0; j < 19; j++){
-			if (tempBoard[i][j] == 3){
-				blocking[count].x = i;
-				blocking[count].y = j;
-				count++;
-			}		
-		}	 
-	}
+void deleteTempMove(int x, int y)
+{
+	tempBoard[x][y] = 0;
 }
 
 
-void changeBlocking(int n, int turn){ //n번째 블로킹을 해당 turn의 돌로 변경
+// 블로킹들의 위치저장 , 구조체 배열에 저장
+void checkBlocking() {
+	int count = 0;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			if (tempBoard[i][j] == 3) {
+				blocking[count].x = i;
+				blocking[count].y = j;
+				count++;
+			}
+		}
+	}
+}
+
+//n번째 블로킹을 해당 turn의 돌로 변경
+void changeBlocking(int n, int turn) {
 
 	int x = blocking[n].x;
 	int y = blocking[n].y;
@@ -132,16 +145,82 @@ void changeBlocking(int n, int turn){ //n번째 블로킹을 해당 turn의 돌로 변경
 }
 
 
-void Minimax()
+//current_depth는 현재 어떤 깊이까지 트리가 진행되었는지를 의미
+//pos_x와 pos_y는 처음 둘 돌의 위치를 저장
+void Minimax(int current_depth, int pos_x, int pos_x1, int pos_y, int pos_y1)
 {
-	battleSearch(tempBoard);			//전장탐색 알고리즘을 통해 battleTop에 우선순위순서로 둘 돌의 위치를 저장합니다.
+	int tempPos_x;
+	int tempPos_y;
+	int tempPos_x1;
+	int tempPos_y1;
+
+	if (current_depth == depth)		//depth까지 내려갔으면 종료
+	{
+		int score = getScore(tempBoard);
+		if (max_score < score)
+		{
+			max_score = score;
+			x[0] = tempPos_x;
+			y[0] = tempPos_y;
+
+			if (cnt > 1)
+			{
+				x[1] = tempPos_x1;
+				y[1] = tempPos_y1;
+			}
+		}
+	}
+
+	else
+	{
+		battleSearch(tempBoard);			//전장탐색 알고리즘을 통해 battleTop에 우선순위순서로 둘 돌의 위치를 저장합니다.
 
 
-	getScore(tempBoard);
+		// 처음 두는 돌의 위치를 저장.
+		if (current_depth == 0)
+		{
+			tempPos_x = battleTop[i][0].x;
+			tempPos_y = battleTop[i][0].y;
 
+			if (cnt > 1)
+			{
+				tempPos_x1 = battleTop[i][1].x;
+				tempPos_y1 = battleTop[i][1].y;
+			}
+		}
+
+		else
+		{
+			tempPos_x = pos_x;
+			tempPos_x1 = pos_x1;
+			tempPos_y = pos_y;
+			tempPos_y1 = pos_y1;
+		}
+
+		//돌두고 Minimax 그리고 돌 지우기
+		for (int i = 0; i < childnum; i++)		//돌을 두어 childnum만큼 tree의 노드를 생성합니다.
+			for (int C = 0; C < cnt; C++)		//cnt가 1이면 한번 두고, 2면 두 번 둡니다.
+			{
+				int turn;
+				turn = (current_depth + 1) % 2;
+				if (turn == 0)
+					turn = 2;
+
+				tempMyMove(battleTop[i][C].x, battleTop[i][C].y, turn);		//임시 판에 돌 두기
+			}
+
+		Minimax(current_depth + 1, tempPos);
+
+
+		for (int C = 0; C < cnt; C++)		//cnt가 1이면 한번 두고, 2면 두 번 둡니다.
+		{
+			deleteTempMove(battleTop[i][C].x, battleTop[i][C].y, turn)	//둔 돌 삭제
+		}
+	}
 }
 
-//qsort함수를 이용하여 상위 3개의 점수를 가진 전장을 찾아내어 배열 battleTop에 저장합니다.
+
+//상위 3개의 점수를 가진 전장을 찾아내어 배열 battleTop에 저장합니다.
 void battleSearch(int tempboard[][])
 {
 
@@ -150,7 +229,8 @@ void battleSearch(int tempboard[][])
 
 }
 
-int getScore(int bd[width][height])	//보드판의 총 점수를 받아오는 함수  - temp와 board
+//보드판의 총 점수를 받아오는 함수  - tempBoard와 board중 선택
+int getScore(int bd[width][height])
 {
 	if (bd == tempBoard)
 	{
@@ -163,6 +243,7 @@ int getScore(int bd[width][height])	//보드판의 총 점수를 받아오는 함수  - temp와
 	}
 }
 
+//판의 점수를 관리하는 함수
 void shapeScore(void) {
 
 }
