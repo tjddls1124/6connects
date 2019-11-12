ed605010e6bb7388195146ff8b6d562cd42f20ef
tjddls1124 <tjddls1124@github.com>
2019-11-12 02:52:20 -0800

    UI update & training

--- QLearing/play6C.py
+++ QLearing/play6C.py
@@ -32,11 +32,11 @@ def showBoard(env):
 	for y in range(gridSize):
 		for x in range(gridSize):
 			if( env.state[y * gridSize + x] == STONE_PLAYER1 ):
-				sys.stdout.write('O')
+				sys.stdout.write(' ○')
 			elif( env.state[y * gridSize + x] == STONE_PLAYER2 ):
-				sys.stdout.write('X')
+				sys.stdout.write(' ●')
 			else:
-				sys.stdout.write('.')
+				sys.stdout.write(' ┿ ')
 		sys.stdout.write('\n')
 	sys.stdout.write('\n')
 
